9ddf69ff25e8df8e557b8cc534fbd0278f46f379
Simha-Stern <a0533187258@gmail.com>
2024-01-02 23:24:37 +0200

    inerface

---
+++ FrontkEnd/src/features/users/usersSlice.ts
@@ -0,0 +1,33 @@
+interface id {
+    id: string
+}
+interface name {
+    first_name: string
+    midel_name?: string
+    last_name: string
+}
+interface address {
+    state?: string
+    country: string
+    city: string
+    streat: string
+    house_number: number
+}
+interface pupel {
+    _id: id
+    email: string
+    pasword: string
+    name: name
+    phone: number
+    phone_password: string
+    address: address
+    active: boolean
+    is_admin: boolean
+    is_deleted: boolean
+}
+interface costumer {
+    _id: id
+    pupel: pupel
+    fee: number
+    fee_payment_method: string
+}
